785fbf0d745719a631ac16932f5dc9be876ec9c1
gareznik <kalesnikau.jerzy@gmail.com>
2026-05-06 21:32:13 +0200

    dodano relacje w BD

--- backend/internal/models/reservation.go
+++ backend/internal/models/reservation.go
@@ -5,7 +5,9 @@ import "time"
 type Reservation struct {
 	ID         uint      `json:"id" gorm:"primaryKey"`
 	UserID     uint      `json:"user_id"`
+	User       User      `json:"-" gorm:"foreignKey:UserID"`
 	AirplaneID uint      `json:"airplane_id"`
+	Airplane   Airplane  `json:"-" gorm:"foreignKey:AirplaneID"`
 	StartTime  time.Time `json:"start_time"`
 	EndTime    time.Time `json:"end_time"`
 }
